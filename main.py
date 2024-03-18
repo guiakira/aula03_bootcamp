20aa3f09b70eed4d94b8ed3f88aed48dab2e5514
Guilherme Akira <akira.enoki@gmail.com>
2024-03-18 20:43:49 -0300

    Adicionando aula de while

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-x = -2
 
-if x <= 0:
-    print("é menor que zero")
-elif x <= 2:
-    print("é menor que dois")
-else:
-    print("More")
+#If
+# x = -2
+
+# if x <= 0:
+#     print("é menor que zero")
+# elif x <= 2:
+#     print("é menor que dois")
+# else:
+#     print("More")
+
+
+#For
+# lista = ["rafael","luciano","fabio"]
+# for aluno in lista:
+#     print(aluno)
+
+#Exemplo
+
+# texto = "hoje é nossa segunda aula do bootcamp, bootcamp de python"
+
+# texto_2 = texto.replace(",","")
+# palavras = texto_2.split(" ")
+
+# contagem_palavras = {}
+
+# for palavra in palavras:
+#     if palavra in contagem_palavras:
+#         contagem_palavras[palavra] = +1
+#     else:
+#         contagem_palavras[palavra] = 1
+
+#while
+import time
+
+condicao = True
+
+while condicao:
+    print("Execute minha ETL")
+    time.sleep(5)
